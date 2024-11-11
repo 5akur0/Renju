7f3570d0d562be37003481d03cbe8db97614024b
5akur0 <2400011411@stu.pku.edu.cn>
2024-11-11 18:59:36 +0800

    The size of the tables shown should be 15-1 * 15 - 1

--- renju.cpp
+++ renju.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int SIZE = 15;
 int board[SIZE][SIZE];
 
-// 1代表黑子(X), 2代表白子(O)
+// 1代表黑子(X)，2代表白子(O)，0代表无子
 void InitializeBoard() {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -15,21 +15,21 @@ void InitializeBoard() {
 
 void PrintBoard() {
 // 上边框
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE - 1; i++) {
         printf("┌───");
     }
     printf("┐\n");
 
     // 中间部分
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = 0; j < SIZE - 1; j++) {
             printf("│   ");
         }
         printf("│\n");
 
         // 画横线
-        if (i < SIZE - 1) {  // 最后一行不需要绘制分隔线
-            for (int j = 0; j < SIZE; j++) {
+        if (i < SIZE - 2) {
+            for (int j = 0; j < SIZE - 1; j++) {
                 printf("├───");
             }
             printf("┤\n");
@@ -37,7 +37,7 @@ void PrintBoard() {
     }
 
     // 画底边框
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE - 1; i++) {
         printf("└───");
     }
     printf("┘\n");
