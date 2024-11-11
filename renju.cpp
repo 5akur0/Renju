b1d3eed995f786f1815fba92bbe0128433910ef9
5akur0 <2400011411@stu.pku.edu.cn>
2024-11-11 20:24:02 +0800

    Done with SetSize

--- renju.cpp
+++ renju.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int SIZE = 15;
-int board[SIZE + 1][SIZE + 1];
+int SIZE = 15;
+int board[51][51];
 // 一行一列冗余，但方便编程
+
+void SetSize() {
+    do {
+        printf("请输入一个介于5到50之间的边长，默认为15（输入0退出）: ");
+        if (scanf("%d", &SIZE) != 1) {
+            while (getchar() != '\n');
+        } else if (SIZE == 0) {
+            printf("程序退出。\n");
+            break;
+        } else if (SIZE < 5 || SIZE > 50) {
+            printf("输入无效，请输入一个介于5到50之间的正整数。\n");
+        }
+    } while (SIZE < 5 || SIZE > 50);
+    printf("边长已设置为%d。\n", SIZE);
+}
 
 // 1代表黑子(●)，2代表白子(◯)，0代表无子
 void InitializeBoard() {
@@ -12,6 +27,13 @@ void InitializeBoard() {
             board[i][j] = 0;
         }
     }
+}
+
+char ToAlpha(int i) {
+    if (i <= 26) {
+        return 'a' + i - 1;
+    }
+    return 'A' + i - 27;
 }
 
 void PrintChar(int i, int j) {
@@ -59,13 +81,15 @@ void PrintBoard() {
     }
     printf("  ");
     for (int j = 1; j <= SIZE; j++) {
-        printf(" %c", 'a' - 1 + j);
+        printf(" %c", ToAlpha(j));
     }
     printf("\n");
 }
 
 int main() {
+    SetSize();
     InitializeBoard();
     PrintBoard();
     return 0;
 }
+
