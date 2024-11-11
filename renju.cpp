04274f1fa2176268ad464cddbb405186adb8cfb5
5akur0 <2400011411@stu.pku.edu.cn>
2024-11-11 18:51:04 +0800

    Done with PrintBoard!

--- renju.cpp
+++ renju.cpp
@@ -1,28 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 15;
-int board[N][N];
+const int SIZE = 15;
+int board[SIZE][SIZE];
 
-void Initialize() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+// 1代表黑子(X), 2代表白子(O)
+void InitializeBoard() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             board[i][j] = 0;
         }
     }
 }
 
-void Print() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
+void PrintBoard() {
+// 上边框
+    for (int i = 0; i < SIZE; i++) {
+        printf("┌───");
     }
+    printf("┐\n");
+
+    // 中间部分
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("│   ");
+        }
+        printf("│\n");
+
+        // 画横线
+        if (i < SIZE - 1) {  // 最后一行不需要绘制分隔线
+            for (int j = 0; j < SIZE; j++) {
+                printf("├───");
+            }
+            printf("┤\n");
+        }
+    }
+
+    // 画底边框
+    for (int i = 0; i < SIZE; i++) {
+        printf("└───");
+    }
+    printf("┘\n");
 }
 
+
 int main() {
-    Initialize();
-    Print();
+    InitializeBoard();
+    PrintBoard();
     return 0;
 }
