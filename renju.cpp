e6b944d010d9ada7bfb47267c368212ca9ab785f
5akur0 <2400011411@stu.pku.edu.cn>
2024-11-11 19:47:18 +0800

    Added Axis, and make the grid look better

--- renju.cpp
+++ renju.cpp
@@ -2,47 +2,67 @@
 using namespace std;
 
 const int SIZE = 15;
-int board[SIZE][SIZE];
+int board[SIZE + 1][SIZE + 1];
+// 一行一列冗余，但方便编程
 
-// 1代表黑子(X)，2代表白子(O)，0代表无子
+// 1代表黑子(●)，2代表白子(◯)，0代表无子
 void InitializeBoard() {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < SIZE + 1; i++) {
+        for (int j = 0; j < SIZE + 1; j++) {
             board[i][j] = 0;
         }
     }
 }
 
-void PrintBoard() {
-// 上边框
-    for (int i = 0; i < SIZE - 1; i++) {
-        printf("┌───");
-    }
-    printf("┐\n");
-
-    // 中间部分
-    for (int i = 0; i < SIZE - 1; i++) {
-        for (int j = 0; j < SIZE - 1; j++) {
-            printf("│   ");
-        }
-        printf("│\n");
-
-        // 画横线
-        if (i < SIZE - 2) {
-            for (int j = 0; j < SIZE - 1; j++) {
-                printf("├───");
+void PrintChar(int i, int j) {
+    if (board[i][j] == 0) {
+        if (i == SIZE) {
+            if (j == 1) {
+                printf("┌─");
+            } else if (j == SIZE) {
+                printf("┐");
+            } else {
+                printf("┬─");
             }
-            printf("┤\n");
+        } else if (i == 1) {
+            if (j == 1) {
+                printf("└─");
+            } else if (j == SIZE) {
+                printf("┘");
+            } else {
+                printf("┴─");
+            }
+        } else {
+            if (j == 1) {
+                printf("├─");
+            } else if (j == SIZE) {
+                printf("┤");
+            } else {
+                printf("┼─");
+            }
         }
+    } else if (board[i][j] == 1) {
+        printf("● ");
+    } else {
+        printf("○ ");
     }
-
-    // 画底边框
-    for (int i = 0; i < SIZE - 1; i++) {
-        printf("└───");
-    }
-    printf("┘\n");
 }
 
+
+void PrintBoard() {
+    for (int i = SIZE; i >= 1; i--) {
+        printf("%2d ", i);
+        for (int j = 1; j <= SIZE; j++) {
+            PrintChar(i, j);
+        }
+        printf("\n");
+    }
+    printf("  ");
+    for (int j = 1; j <= SIZE; j++) {
+        printf(" %c", 'a' - 1 + j);
+    }
+    printf("\n");
+}
 
 int main() {
     InitializeBoard();
